decc1385c4b24782ac37c8d30fd1e2963469654c
Lachlan McDonald <lachlan@radelaide.net>
2019-11-19 23:07:23 +0000

    CHORE Readme and compress script

---
+++ README.md
@@ -0,0 +1,6 @@
+# ThickerLines Factorio Mod
+
+Factorio Mod which thickens power-lines and circuitry to improve visiblity.
+
+- Tested on Factorio version 0.17
+- [Review open issues](https://github.com/lachlanmcdonald/factorio-mod-thicker-lines/issues) or [create a new issue](https://github.com/lachlanmcdonald/factorio-mod-thicker-lines/issues/new)
---
+++ compress.sh
@@ -0,0 +1,16 @@
+ZIP_FILE=`cat "./info.json" | jq --raw-output '(.name) + "_" + (.version) + ".zip"'`
+zip -r -X $ZIP_FILE \
+	"./info.json" \
+	"./thumbnail.png" \
+	"./data-updates.lua" \
+	"./LICENSE" \
+	"./core/graphics/hr-green-wire.png" \
+	"./core/graphics/hr-wire-shadow.png" \
+	"./core/graphics/hr-red-wire.png" \
+	"./core/graphics/red-wire.png" \
+	"./core/graphics/green-wire.png" \
+	"./core/graphics/wire-shadow.png" \
+	"./core/graphics/hr-copper-wire.png" \
+	"./core/graphics/copper-wire.png" \
+	"./core/graphics/wire-highlight.png" \
+	"./core/graphics/hr-wire-highlight.png" \
