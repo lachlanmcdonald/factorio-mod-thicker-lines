af85b8425e95d99663a689477313849c48f653f5
Lachlan McDonald <lachlan@radelaide.net>
2020-10-18 15:43:48 +0100

    FIX

--- compress.ps1
+++ compress.ps1
@@ -4,17 +4,18 @@ $modVersion = ( $modInfo | Select-Object -ExpandProperty 'version' )
 $package = "$($modName)_$($modVersion)"
 
 New-Item -Path $package -ItemType 'directory' -Force
+New-Item -Path "$package/core/graphics" -ItemType 'directory' -Force
 
 @(
 	'info.json',
 	'changelog.txt',
 	'thumbnail.png',
 	'data-updates.lua',
-	'LICENSE',
-	"core/graphics"
+	'LICENSE'
 ).ForEach({
-	Write-Output($_)
 	Copy-Item -Path $_ -Recurse -Container -Destination $package
 })
 
-Compress-Archive -Path $package  -DestinationPath "$($package).zip" -CompressionLevel Optimal
+Copy-Item -Path "core/graphics/*.png" -Destination "$package/core/graphics"
+
+Compress-Archive -Path $package -DestinationPath "$($package).zip" -CompressionLevel Optimal
