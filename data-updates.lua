37674e0350209a7560cb90da640f26fe8586333d
Clarence "Sparr" Risher <sparr0@gmail.com>
2022-02-09 18:01:29 -0500

    Add options for individual wire types. Fix metadata.

--- data-updates.lua
+++ data-updates.lua
@@ -1,16 +1,23 @@
 local core_graphics = "__ThickerLines__/core/graphics"
+local sprites = data.raw["utility-sprites"]["default"]
 
-data.raw["utility-sprites"]["default"]["copper_wire"].filename                      = core_graphics .. "/copper-wire.png"
-data.raw["utility-sprites"]["default"]["copper_wire"].hr_version.filename           = core_graphics .. "/hr-copper-wire.png"
-data.raw["utility-sprites"]["default"]["green_wire"].filename                       = core_graphics .. "/green-wire.png"
-data.raw["utility-sprites"]["default"]["green_wire"].hr_version.filename            = core_graphics .. "/hr-green-wire.png"
-data.raw["utility-sprites"]["default"]["red_wire"].filename                         = core_graphics .. "/red-wire.png"
-data.raw["utility-sprites"]["default"]["red_wire"].hr_version.filename              = core_graphics .. "/hr-red-wire.png"
-data.raw["utility-sprites"]["default"]["wire_shadow"].filename                      = core_graphics .. "/wire-shadow.png"
-data.raw["utility-sprites"]["default"]["wire_shadow"].hr_version.filename           = core_graphics .. "/hr-wire-shadow.png"
-data.raw["utility-sprites"]["default"]["wire_shadow"].filename                      = core_graphics .. "/wire-shadow.png"
-data.raw["utility-sprites"]["default"]["wire_shadow"].hr_version.filename           = core_graphics .. "/hr-wire-shadow.png"
-data.raw["utility-sprites"]["default"]["green_wire_hightlight"].filename            = core_graphics .. "/wire-highlight.png"
-data.raw["utility-sprites"]["default"]["green_wire_hightlight"].hr_version.filename = core_graphics .. "/hr-wire-highlight.png"
-data.raw["utility-sprites"]["default"]["red_wire_hightlight"].filename              = core_graphics .. "/wire-highlight.png"
-data.raw["utility-sprites"]["default"]["red_wire_hightlight"].hr_version.filename   = core_graphics .. "/hr-wire-highlight.png"
+if (settings.startup["thicker-lines-copper"].value) then
+  sprites["copper_wire"].filename                      = core_graphics .. "/copper-wire.png"
+  sprites["copper_wire"].hr_version.filename           = core_graphics .. "/hr-copper-wire.png"
+end
+
+if (settings.startup["thicker-lines-circuit"].value) then
+  sprites["green_wire"].filename                       = core_graphics .. "/green-wire.png"
+  sprites["green_wire"].hr_version.filename            = core_graphics .. "/hr-green-wire.png"
+  sprites["red_wire"].filename                         = core_graphics .. "/red-wire.png"
+  sprites["red_wire"].hr_version.filename              = core_graphics .. "/hr-red-wire.png"
+  sprites["green_wire_hightlight"].filename            = core_graphics .. "/wire-highlight.png"
+  sprites["green_wire_hightlight"].hr_version.filename = core_graphics .. "/hr-wire-highlight.png"
+  sprites["red_wire_hightlight"].filename              = core_graphics .. "/wire-highlight.png"
+  sprites["red_wire_hightlight"].hr_version.filename   = core_graphics .. "/hr-wire-highlight.png"
+end
+
+if (settings.startup["thicker-lines-shadow"].value) then
+  sprites["wire_shadow"].filename                      = core_graphics .. "/wire-shadow.png"
+  sprites["wire_shadow"].hr_version.filename           = core_graphics .. "/hr-wire-shadow.png"
+end
