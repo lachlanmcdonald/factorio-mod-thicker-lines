dbf0f92d166b40ef3f468a6b049793a26c69f65a
Lachlan McDonald <lachlan@radelaide.net>
2022-02-13 12:54:37 +0000

    FIX

--- settings.lua
+++ settings.lua
@@ -1,23 +1,21 @@
-data:extend {
-    {
-        name = 'thicker-lines-copper',
-        setting_type = 'startup',
-        type = 'bool-setting',
-        default_value = true,
-        order = "a",
-    },
-    {
-        name = 'thicker-lines-circuit',
-        setting_type = 'startup',
-        type = 'bool-setting',
-        default_value = true,
-        order = "b",
-    },
-    {
-        name = 'thicker-lines-shadow',
-        setting_type = 'startup',
-        type = 'bool-setting',
-        default_value = true,
-        order = "c",
-    },
-}
+data:extend({
+	{
+		name = 'thicker-lines-copper',
+		setting_type = 'startup',
+		type = 'bool-setting',
+		default_value = true,
+		order = "a",
+	}, {
+		name = 'thicker-lines-circuit',
+		setting_type = 'startup',
+		type = 'bool-setting',
+		default_value = true,
+		order = "b",
+	}, {
+		name = 'thicker-lines-shadow',
+		setting_type = 'startup',
+		type = 'bool-setting',
+		default_value = true,
+		order = "c",
+	},
+})
