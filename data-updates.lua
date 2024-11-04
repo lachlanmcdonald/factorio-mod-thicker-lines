01dfd87fa072133bcaee5bc0192ff8d1b48aaa9b
Lachlan McDonald <lachlanmcdonald@users.noreply.github.com>
2024-11-04 23:17:04 +0000

    Squashed commit of the following:
    commit 74c6325340c07afee4b5d8cc84c1773fe8e581f1
    Author: Lachlan McDonald <lachlanmcdonald@users.noreply.github.com>
    Date:   Mon Nov 4 23:16:38 2024 +0000
    
        FEAT Updates for 2.0

--- data-updates.lua
+++ data-updates.lua
@@ -2,22 +2,17 @@ local core_graphics = "__ThickerLines__/core/graphics"
 local sprites = data.raw["utility-sprites"]["default"]
 
 if (settings.startup["thicker-lines-copper"].value) then
-	sprites["copper_wire"].filename                      = core_graphics .. "/copper-wire.png"
-	sprites["copper_wire"].hr_version.filename           = core_graphics .. "/hr-copper-wire.png"
+	sprites["copper_wire"].filename           = core_graphics .. "/copper-wire.png"
+	sprites["copper_wire_highlight"].filename = core_graphics .. "/wire-highlight.png"
 end
 
 if (settings.startup["thicker-lines-circuit"].value) then
-	sprites["green_wire"].filename                       = core_graphics .. "/green-wire.png"
-	sprites["green_wire"].hr_version.filename            = core_graphics .. "/hr-green-wire.png"
-	sprites["red_wire"].filename                         = core_graphics .. "/red-wire.png"
-	sprites["red_wire"].hr_version.filename              = core_graphics .. "/hr-red-wire.png"
-	sprites["green_wire_hightlight"].filename            = core_graphics .. "/wire-highlight.png"
-	sprites["green_wire_hightlight"].hr_version.filename = core_graphics .. "/hr-wire-highlight.png"
-	sprites["red_wire_hightlight"].filename              = core_graphics .. "/wire-highlight.png"
-	sprites["red_wire_hightlight"].hr_version.filename   = core_graphics .. "/hr-wire-highlight.png"
+	sprites["green_wire"].filename            = core_graphics .. "/green-wire.png"
+	sprites["red_wire"].filename              = core_graphics .. "/red-wire.png"
+	sprites["green_wire_highlight"].filename = core_graphics .. "/wire-highlight.png"
+	sprites["red_wire_highlight"].filename   = core_graphics .. "/wire-highlight.png"
 end
 
 if (settings.startup["thicker-lines-shadow"].value) then
-	sprites["wire_shadow"].filename                      = core_graphics .. "/wire-shadow.png"
-	sprites["wire_shadow"].hr_version.filename           = core_graphics .. "/hr-wire-shadow.png"
+	sprites["wire_shadow"].filename           = core_graphics .. "/wire-shadow.png"
 end
